e54eec64de4b280d4b8d6fda8ece315e84f0c012
An <162073486+AnDayno@users.noreply.github.com>
2025-09-17 14:29:11 +0800

    Added lerping to rotation

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,18 +8,23 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float xClampRange = 5f;
     [SerializeField] float yClampRange = 5f;
 
+    [SerializeField] float controlRollFactor = 20f;
+    [SerializeField] float rotationSpeed = 5f;
+
     Vector2 movement;
 
     void Update()
     {
         ProcessTranslation();
+        ProcessRotation();
     }
 
     public void OnMove(InputValue value)
     {
         movement = value.Get<Vector2>();
     }
-    private void ProcessTranslation()
+
+    void ProcessTranslation()
     {
         float xOffset = movement.x * controlSpeed * Time.deltaTime;
         float rawXpos = transform.localPosition.x + -xOffset;
@@ -29,5 +35,11 @@ public class PlayerMovement : MonoBehaviour
         float clampedYpos = Mathf.Clamp(rawYpos, -yClampRange, yClampRange);
 
         transform.localPosition = new Vector3(clampedXpos, clampedYpos, 0f);
+    }
+
+    void ProcessRotation()
+    {
+        Quaternion targetRotation = Quaternion.Euler(0f, 0f, controlRollFactor * movement.x);
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 }
