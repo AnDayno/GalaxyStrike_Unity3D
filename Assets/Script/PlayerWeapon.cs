312b2f5df90dc8a3fd1e04c23f3b0fb51186be94
An <162073486+AnDayno@users.noreply.github.com>
2025-09-17 20:46:02 +0800

    Added crosshair

--- Assets/Script/PlayerWeapon.cs
+++ Assets/Script/PlayerWeapon.cs
@@ -5,17 +5,19 @@ using UnityEngine.InputSystem;
 public class PlayerWeapon : MonoBehaviour
 {
     [SerializeField] GameObject[] lasers;
+    [SerializeField] RectTransform crosshair;
 
     bool isFiring = false;
 
     void Start()
     {
-        
+        Cursor.visible = false;
     }
 
     void Update()
     {
         processFiring();
+        MoveCrosshair();
     }
 
     public void OnFire(InputValue value)
@@ -30,5 +32,10 @@ public class PlayerWeapon : MonoBehaviour
             var emissionModule = laser.GetComponent<ParticleSystem>().emission;
             emissionModule.enabled = isFiring;
         }
+    }
+
+    void MoveCrosshair()
+    {
+        crosshair.position = Mouse.current.position.ReadValue();
     }
 }
