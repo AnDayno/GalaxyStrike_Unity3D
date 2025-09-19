bd35cb77df3cc578aa4466e212a8a95261620dae
An <162073486+AnDayno@users.noreply.github.com>
2025-09-19 14:29:11 +0800

    Added scoreboard

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -4,6 +4,15 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject destroyedVFX;
     [SerializeField] int hitPoints = 3;
+    [SerializeField] int scoreValue = 10;
+
+    Scoreboard scoreboard;
+
+    private void Start()
+    {
+        scoreboard = FindFirstObjectByType<Scoreboard>();
+    }
+
     private void OnParticleCollision(GameObject other)
     {
         ProcessHit();
@@ -14,6 +23,7 @@ public class Enemy : MonoBehaviour
         hitPoints--;
         if (hitPoints <= 0)
         {
+            scoreboard.IncreaseScore(scoreValue);
             Instantiate(destroyedVFX, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
